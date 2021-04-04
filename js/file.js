055956501ffb2bbcd9cf071867380a04d49dec8c
Aya Osama <aya55osma2001@gmail.com>
2021-04-04 19:21:23 +0200

    kkbjkbjsdf

--- js/file.js
+++ js/file.js
@@ -2,57 +2,6 @@ let loadPage = document.querySelector(".load");
 window.onload = function () {
     loadPage.style.display = "none";
 }
-/* shop cart */
-let shopCart = document.querySelector(".cart");
-let closeIcon = document.querySelector(".close-icon");
-let yourCart = document.querySelector(".your-cart");
-shopCart.addEventListener("click" , function () {
-    yourCart.style.right = "0";
-});
-closeIcon.addEventListener("click" , function () {
-    yourCart.style.right = "-100%";
-});
-/*****************************/
-let searchicon = document.querySelector(".searchicon");
-let icon_close = document.querySelector(".closeicon");
-let searchbox = document.querySelector(".search-box");
-searchicon.addEventListener("click", function() {
-    //searchbox.style.top = "0";
-    searchbox.style.display = "block";
-});
-icon_close.addEventListener("click", function() {
-    //searchbox.style.top = "-100%";
-    searchbox.style.display = "none";
-});
-/********************************/
-let ourHeader = document.querySelector(".header-section");
-let imgArray = ['slide-01.jpg' , 'slide-02.jpg' , 
-'slide-03.jpg'];
-let content1 = document.querySelector(".one");
-let content2 = document.querySelector(".two");
-let content3 = document.querySelector(".three");
-let contentArray = [content1, content2, content3];
-let i = 0;
-setInterval(function () {
-    ourHeader.style.backgroundImage = 'url("images/' + imgArray[i] + '")';
-    if (i===0){
-        contentArray[1].style.display = "none";
-        contentArray[2].style.display = "none";
-        contentArray[0].style.display = "block";
-        i = i+1;
-    } else if (i===1){
-        contentArray[1].style.display = "block";
-        contentArray[2].style.display = "none";
-        contentArray[0].style.display = "none";
-        i = i+1;
-    } else if (i===2){
-        contentArray[1].style.display = "none";
-        contentArray[2].style.display = "block";
-        contentArray[0].style.display = "none";
-        i = 0;
-    }
-}, 5000);
-
 let navBar = document.querySelector(".nav-bar");
 let toTopButton = document.querySelector(".to-top");
 window.onscroll = function () {
@@ -109,6 +58,58 @@ myListIcon.addEventListener("click", function () {
         myListIcon.classList.add("fa-bars");
     }
 });
+/* shop cart */
+let shopCart = document.querySelector(".cart");
+let closeIcon = document.querySelector(".close-icon");
+let yourCart = document.querySelector(".your-cart");
+shopCart.addEventListener("click" , function () {
+    yourCart.style.right = "0";
+});
+closeIcon.addEventListener("click" , function () {
+    yourCart.style.right = "-100%";
+});
+/*****************************/
+let searchicon = document.querySelector(".searchicon");
+let icon_close = document.querySelector(".closeicon");
+let searchbox = document.querySelector(".search-box");
+searchicon.addEventListener("click", function() {
+    //searchbox.style.top = "0";
+    searchbox.style.display = "block";
+});
+icon_close.addEventListener("click", function() {
+    //searchbox.style.top = "-100%";
+    searchbox.style.display = "none";
+});
+/********************************/
+let ourHeader = document.querySelector(".header-section");
+let imgArray = ['slide-01.jpg' , 'slide-02.jpg' , 
+'slide-03.jpg'];
+let content1 = document.querySelector(".one");
+let content2 = document.querySelector(".two");
+let content3 = document.querySelector(".three");
+let contentArray = [content1, content2, content3];
+let i = 0;
+setInterval(function () {
+    ourHeader.style.backgroundImage = 'url("images/' + imgArray[i] + '")';
+    if (i===0){
+        contentArray[1].style.display = "none";
+        contentArray[2].style.display = "none";
+        contentArray[0].style.display = "block";
+        i = i+1;
+    } else if (i===1){
+        contentArray[1].style.display = "block";
+        contentArray[2].style.display = "none";
+        contentArray[0].style.display = "none";
+        i = i+1;
+    } else if (i===2){
+        contentArray[1].style.display = "none";
+        contentArray[2].style.display = "block";
+        contentArray[0].style.display = "none";
+        i = 0;
+    }
+}, 5000);
+
+
 /*****************************************************/
 
 let searchBox = document.querySelector(".overview-product .container > .search");
